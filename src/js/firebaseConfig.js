bac135863c1cbb329a8dcad3a69ad1ed63454bab
Annettehv <anha040@student.kristiania.no>
2024-05-16 14:09:31 +0200

    connected to firebase

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBrMSpoWhA3ZyMkBXv9dNSO_8j2ktdJqjY",
+	authDomain: "finalproject-8a6f6.firebaseapp.com",
+	projectId: "finalproject-8a6f6",
+	storageBucket: "finalproject-8a6f6.appspot.com",
+	messagingSenderId: "329916265962",
+	appId: "1:329916265962:web:9d63823ff7ced2e8882e93"
+  };
+
+  export default firebaseConfig
